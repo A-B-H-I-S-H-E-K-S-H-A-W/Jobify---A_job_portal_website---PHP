e986dea40cf3b623ad61ce32fd2c0c4f9838624c
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2024-10-27 19:22:38 +0530

    Retrieve data from database in job page

--- jobs.php
+++ jobs.php
@@ -1,3 +1,7 @@
+<?php 
+include("admin/db/db.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -17,26 +21,65 @@
   <body>
     <?php include("includes/header.php"); ?>
     <div class="max-w-[1280px] mx-auto px-10">
-        <div class="relative grid grid-cols-2 mt-20 gap-5">
-            <div class="sticky top-20 max-h-[70vh] rounded-xl ring-1 ring-gray-400 shadow-xl">
-                <div class=" "></div>
-            </div>
-            <div class="grid grid-cols-1 gap-5 shadow-xl">
-                <div class="h-80 ring-1 ring-gray-400 p-6 rounded-xl flex flex-col gap-3 items-start">
-                    <div>
-                        <h3 class="text-2xl text-blue-800 font-bold">Job Role</h3>
-                        <p>Accenture</p>
-                        <p>Kolkata, West Bengal</p>
-                    </div>
-                    <span class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">Full Time</span>
+          <div>
+            <h2 class="text-3xl font-bold text-gray-600 my-16">All Available Jobs</h2>
+          </div>
+    
+          <?php
+          // Fetch job listings
+          $sel = "SELECT recruiter.cname, recruiter.email, jobs.* 
+                  FROM recruiter 
+                  INNER JOIN jobs ON recruiter.cid = jobs.rid";  
+          $rs = $con->query($sel);
+          while ($row = $rs->fetch_assoc()) {
+          ?>
+          <!-- Job Button -->
 
-                    <div class="mini-description text-sm flex">
-                        <p>Project Role : Software Development Engineer Project Role Description : Analyze, design, code and test multiple components of application code across one or more clients. Perform maintenance, enhancements and/or development work. Must have Skills : SAP UI5 Development Good to Have Skills : Job Requirements : Key Responsibilities : Summary:As an Application Developer, you will design, build, and configure applications to meet business process and application requirements. </p>
-                    </div>
-                    <div><p class="text-gray-400 text-sm">Posted 4 days ago</p></div>
+          
+          
+          <div  class="text-start">
+            <div class="grid grid-cols-1 gap-5 shadow-xl mt-5">
+              <div class="h-full ring-1 ring-gray-400 p-6 rounded-xl flex flex-col gap-3">
+              <a href="perjob.php?jid=<?php echo $row['jid'] ?>">
+                <div class="flex flex-col items-start">
+                
+                  <div class="flex justify-between w-full">
+                    <h3 class="text-2xl text-blue-800 font-bold"><?php echo $row['role']; ?>, Exprience : <?php echo $row['exp']; ?></h3>
+                    <p class="px-4 py-2 rounded-lg bg-blue-900 text-white font-semibold hover:ring-1 hover:ring-blue-900 hover:bg-white hover:text-blue-900 duration-200 ease-linear">Show Details</p>
+                  </div>
+                  <p><?php echo $row['cname']; ?></p>
+                  <p><?php echo $row['city']; ?>, <?php echo $row['state']; ?></p>
+                  
                 </div>
+                </a>
+                <div class="flex items-start">
+                  <span class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10"><?php echo $row['type']; ?></span>
+                </div>
+                <div class="mini-description text-sm flex">
+                  <p>Project Role : 
+                    <?php
+                    if (strlen($row['about']) > 300) {
+                      echo substr($row['about'], 0, 300) . "...";
+                    } else {
+                      echo $row['about'];
+                    }
+                    ?>
+                  </p>
+                </div>
+                <div><p class="text-gray-700 text-sm">Salary: 
+                  <?php
+                  if ($row['min_salary'] == 0 && $row['max_salary'] == 0) {
+                    echo "Not disclosed";
+                  } else {
+                    echo "₹ " . $row['min_salary'] . " - ₹ " . $row['max_salary'];
+                  }
+                  ?>
+                </p></div>
+                <div><p class="text-gray-400 text-sm">Posted 4 days ago</p></div>
+              </div>
             </div>
-        </div>
+          </div>
+          <?php } ?>
     </div>
 
     <?php include("includes/footer.php"); ?>
