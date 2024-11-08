aa7a4b75d70abdea689278c89747ae00e42c9e6a
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2024-11-08 11:18:00 +0530

    Made dynamic changes to comapnies page

--- companies.php
+++ companies.php
@@ -41,6 +41,8 @@ if(isset($_SESSION['email'])){
           <div  class="text-start">
             <div class="grid grid-cols-1 gap-5 shadow-xl mt-5">
               <div class="h-full ring-1 ring-gray-400 p-6 rounded-xl flex flex-col gap-3">
+
+              <?php if(isset($_SESSION['email'])){ ?>
               <a href="perjob.php?jid=<?php echo $row['cid'] ?>">
                 <div class="flex flex-col items-start">
                 
@@ -64,12 +66,60 @@ if(isset($_SESSION['email'])){
                   
                 </div>
                 </a>
+                <?php } else { ?>
+                      <div class="flex flex-col items-start">
+                      
+                        <div class="flex justify-between w-full">
+                          <h3 class="text-2xl text-blue-800 font-bold"><?php echo $row['cname']; ?></h3>
+                          <button onclick="openModal('modelConfirm')"  class="px-4 py-2 rounded-lg bg-blue-900 text-white font-semibold hover:ring-1 hover:ring-blue-900 hover:bg-white hover:text-blue-900 duration-200 ease-linear">About Company</button>
+                        </div>
+                        <p><?php echo $row['city']; ?>, <?php echo $row['state']; ?></p>
+                        <p>Total job openings : <?php
+                        $count = 0; 
+                        $cid = $row['cid'];
+                        $sel = "SELECT * FROM jobs WHERE rid='$cid'";
+                        $rs = $con->query($sel);
+                        while ($row = $rs->fetch_assoc()) {
+                          if($cid == $row['rid']){
+                            $count += 1;
+                          } 
+                        }
+                        echo $count;
+                        ?></p>
+                        
+                      </div>
+                <?php } ?>
               </div>
             </div>
           </div>
           <?php } ?>
+
+          <!-- Model Box -->
+
+          <div id="modelConfirm" class="fixed hidden top-0 z-50 inset-0 h-full w-full px-4">
+                <div class="relative top-10 mx-auto shadow-2xl rounded-md border bg-white max-w-md">
+                    <div class="flex justify-end p-2">
+                        <button onclick="closeModal('modelConfirm')" type="button"
+                            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1 ml-auto inline-flex items-center">
+                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                clip-rule="evenodd"></path>
+                            </svg>
+                        </button>
+                    </div>
+
+                    <div class="p-6 pt-0 ">
+                        <h2 class="text-2xl text-center font-bold mb-10 text-blue-900">Alert Message from Jobify</h2>
+                        <!-- Content Area -->
+                        <span class="text-start text-lg font-medium text-red-700 p-2 rounded-lg">Warning : Login First to Access</span>
+                    </div>
+                </div>
+            </div>
+
     </div>
 
     <?php include("includes/footer.php"); ?>
+    <script src="index.js" type="text/javascript" ></script>
   </body>
 </html>
