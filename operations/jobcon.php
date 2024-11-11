ead207295855299368715184e61d8ab662ca7fbe
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2024-11-11 22:06:38 +0530

    Minor Chnages

--- operations/jobcon.php
+++ operations/jobcon.php
@@ -7,11 +7,5 @@ $jid = $_GET['jid'];
 $upd = "UPDATE user SET conid='$jid' WHERE uid='$uid' ";
 $con->query($upd);
 
-$sel = "SELECT * FROM user WHERE uid='$uid'";
-$rs = $con->query($sel);
-$row = $rs->fetch_assoc();
-
-$_SESSION['conid'] = $row['conid'];
-
 header("location:../jobDetails.php?jid=$jid");
 ?>
